03a68a7b5cb9d2588d04f3c8ad888c8e910269aa
pavitroBweb <pavitro.bweb>
2018-09-12 17:33:42 +0530

    change scroll response with position to button.

--- app/src/main/java/com/example/bcs/discretescroll/MainActivity.java
+++ app/src/main/java/com/example/bcs/discretescroll/MainActivity.java
@@ -55,13 +55,13 @@ public class MainActivity extends AppCompatActivity implements DiscreteScrollVie
 
     private Integer[] IMAGES = {R.drawable.employee1, R.drawable.employee1, R.drawable.employee1};
     private ArrayList<Integer> arrayList;
+    RecyclerViewAdapter adapter;
+    int i;
+    int k;
 
-//    LinearLayoutManager linearLayoutManager=(LinearLayoutManager)recyclerView.getLayoutManager();
-
-
+    
     private static final String TAG = "MainActivity";
 
-    //vars
     private ArrayList<String> mNames = new ArrayList<>();
     private ArrayList<Integer> mImageUrls = new ArrayList<>();
 
@@ -74,67 +74,17 @@ public class MainActivity extends AppCompatActivity implements DiscreteScrollVie
         imgbtn2=(ImageButton)findViewById(R.id.frontButton);
         imgbtn1.setVisibility(View.INVISIBLE);
 
-
-       // dotsLayout = (LinearLayout) findViewById(R.id.layoutDots);
-
-//        currentItemName = (TextView) findViewById(R.id.item_name);
-//        currentItemPrice = (TextView) findViewById(R.id.item_price);
-//        rateItemButton = (ImageView) findViewById(R.id.item_btn_rate);
-
         shop = Shop.get();
         data = shop.getData();
-//        itemPicker = (DiscreteScrollView) findViewById(R.id.item_picker);
-//        itemPicker.setOrientation(DSVOrientation.HORIZONTAL);
-//        itemPicker.addOnItemChangedListener(this);
-//        infiniteAdapter = InfiniteScrollAdapter.wrap(new ShopAdapter(data));
-//        itemPicker.setAdapter(infiniteAdapter);
-////        itemPicker.setItemTransitionTimeMillis(DiscreteScrollViewOptions.getTransitionTime());
-//        itemPicker.setItemTransformer(new ScaleTransformer.Builder()
-//                .setMinScale(0.8f)
-//                .build());
 
         getImages();
-
-       // addBottomDots(0);
         arrayList=new ArrayList<>();
         arrayList=populateList();
         init();
 
-
-//        onItemChanged(data.get(0));
-
-//        findViewById(R.id.item_btn_rate).setOnClickListener(this);
-//        findViewById(R.id.item_btn_buy).setOnClickListener(this);
-//        findViewById(R.id.item_btn_comment).setOnClickListener(this);
-
-//        findViewById(R.id.home).setOnClickListener(this);
-//        findViewById(R.id.btn_smooth_scroll).setOnClickListener(this);
-//        findViewById(R.id.btn_transition_time).setOnClickListener(this);
     }
-
-
-
-//    private void addBottomDots(int currentPage) {
-//        dotsCount = mImageUrls.size();
-//        System.out.println("count"+dotsCount);
-//        dots = new ImageView[dotsCount];
-//
-//        dotsLayout.removeAllViews();
-//        for (int i = 0; i < dotsCount; i++) {
-//            dots[i] = new ImageView(this);
-//            dots[i].setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.active_dot));
-//            dots[i].setPadding(10,0,10,0);
-//            dotsLayout.addView(dots[i]);
-//        }
-//
-//        dots[0].setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.active_dot));
-////        if (dots.length > 0)
-////            dots[currentPage].setTextColor(colorsActive[currentPage]);
-//    }
-
     @Override
     public void onClick(View v) {
-        LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
         switch (v.getId()) {
 
             case R.id.backButton:
@@ -144,11 +94,11 @@ public class MainActivity extends AppCompatActivity implements DiscreteScrollVie
                 int k=mImageUrls.size()-1;
                 int l=llb.findLastVisibleItemPosition()-1;
                 Log.e("backPosition", String.valueOf(l));
-                if (l==1){
+                if (i==0){
                     imgbtn1.setVisibility(View.INVISIBLE);
                     imgbtn2.setVisibility(View.VISIBLE);
 
-                }else if (l>0){
+                }else if (i>=0){
                     imgbtn1.setVisibility(View.VISIBLE);
                     imgbtn2.setVisibility(View.VISIBLE);
                 }
@@ -158,55 +108,26 @@ public class MainActivity extends AppCompatActivity implements DiscreteScrollVie
                 LinearLayoutManager llm = (LinearLayoutManager) recyclerView.getLayoutManager();
                 llm.scrollToPosition(llm.findLastVisibleItemPosition() +1);
 
-                int j=mImageUrls.size()-1;
-                int i=llm.findLastVisibleItemPosition();
+                k=mImageUrls.size()-1;
                 Log.e("position", String.valueOf(i));
-                Log.e("total size", String.valueOf(j));
-
-                if (i==j){
+                if (i==k){
                     imgbtn2.setVisibility(View.INVISIBLE);
                     imgbtn1.setVisibility(View.VISIBLE);
                 } else if (i>0){
                     imgbtn1.setVisibility(View.VISIBLE);
                 }
                 break;
-//            case R.id.item_btn_rate:
-//                int realPosition = infiniteAdapter.getRealPosition(itemPicker.getCurrentItem());
-//                Item current = data.get(realPosition);
-//                shop.setRated(current.getId(), !shop.isRated(current.getId()));
-//                changeRateButtonState(current);
-//                break;
             case R.id.home:
                 finish();
                 break;
 
-//            case R.id.btn_transition_time:
-//                DiscreteScrollViewOptions.configureTransitionTime(itemPicker);
-//                break;
-//            case R.id.btn_smooth_scroll:
-//                DiscreteScrollViewOptions.smoothScrollToUserSelectedPosition(itemPicker, v);
-//                break;
             default:
                 showUnsupportedSnackBar();
                 break;
         }
     }
 
-//    private void onItemChanged(Item item) {
-//        currentItemName.setText(item.getName());
-//        currentItemPrice.setText(item.getPrice());
-////        changeRateButtonState(item);
-//    }
 
-//    private void changeRateButtonState(Item item) {
-//        if (shop.isRated(item.getId())) {
-//            rateItemButton.setImageResource(R.drawable.ic_star_black_24dp);
-//            rateItemButton.setColorFilter(ContextCompat.getColor(this, R.color.shopRatedStar));
-//        } else {
-//            rateItemButton.setImageResource(R.drawable.ic_star_border_black_24dp);
-//            rateItemButton.setColorFilter(ContextCompat.getColor(this, R.color.shopSecondary));
-//        }
-//    }
 
     @Override
     public void onCurrentItemChanged(@Nullable RecyclerView.ViewHolder viewHolder, int position) {
@@ -261,16 +182,43 @@ public class MainActivity extends AppCompatActivity implements DiscreteScrollVie
         LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
         recyclerView = findViewById(R.id.recyclerView);
         recyclerView.setLayoutManager(layoutManager);
-        RecyclerViewAdapter adapter = new RecyclerViewAdapter(this, mNames, mImageUrls);
+        adapter = new RecyclerViewAdapter(this, mNames, mImageUrls);
         recyclerView.setAdapter(adapter);
+        recyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
+                super.onScrollStateChanged(recyclerView, newState);
+
+
+            }
+
+            @Override
+            public void onScrolled(RecyclerView recyclerView1, int dx, int dy) {
+                super.onScrolled(recyclerView1, dx, dy);
+                LinearLayoutManager llm = (LinearLayoutManager) recyclerView.getLayoutManager();
+                 i =llm.findFirstCompletelyVisibleItemPosition();
+
+                 k=mImageUrls.size()-1;
+                Log.e("totalPosition", String.valueOf(k));
+                Log.e("firstPosition", String.valueOf(i));
+                if (i==k ){
+                    imgbtn2.setVisibility(View.INVISIBLE);
+                    imgbtn1.setVisibility(View.VISIBLE);
+                } else if (i>0 && i<k){
+                    imgbtn1.setVisibility(View.VISIBLE);
+                    imgbtn2.setVisibility(View.VISIBLE);
+                }else if (i==0){
+                    imgbtn1.setVisibility(View.INVISIBLE);
+                }
+            }
+        });
     }
     private ArrayList<Integer> populateList(){
 
         ArrayList<Integer> list = new ArrayList<>();
 
         for(int i = 0; i < IMAGES.length; i++){
-//            ImageModel imageModel = new ImageModel();
-//            imageModel.setImage_drawable(myImageList[i]);
+
 
             list.add(IMAGES[i]);
         }
@@ -279,8 +227,6 @@ public class MainActivity extends AppCompatActivity implements DiscreteScrollVie
     }
 
     public void init() {
-//        for (int i = 0; i < IMAGES.length; i++)
-//            arrayList.add(IMAGES[i]);
 
             PagerContainer mContainer = (PagerContainer) findViewById(R.id.pager_container);
 
